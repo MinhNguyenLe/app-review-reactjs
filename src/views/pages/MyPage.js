fed4ecf89a01805972c3bce25ff9f0b6cb971f17
ZinZinNguyen <71427292+ZinZinNguyen@users.noreply.github.com>
2021-06-23 11:57:37 +0700

    icon add img

--- src/views/pages/MyPage.js
+++ src/views/pages/MyPage.js
@@ -10,6 +10,7 @@ import Review from "components/review/Review";
 import * as rb from "react-bootstrap";
 import TextareaAutosize from "react-textarea-autosize";
 import * as func from "javascript/funcGlobal.js";
+import UpLoadImg from "components/input/UploadImg";
 
 import {
   Button,
@@ -75,10 +76,10 @@ function MyPage() {
         axios.get(`${apiLocal}/api/reviews/users/${user.id}`),
       ])
         .then(([cmt, re]) => {
-          $(`#loading_1_${user.id}`).addClass("hidden");
+           $(`#loading_1_${user.id}`).addClass("hidden");
           $(`#loading_2_${user.id}`).addClass("hidden");
-          if(!re.data.length) $(".text-after-loading-re").prepend( "<h2>0</h2>" )
-          if(!cmt.data.length) $(".text-after-loading-cmt").prepend( "<h2>0</h2>" )
+          // if(!re.data.length && success === 0) $(".text-after-loading-re").prepend( `<h2 className="one">0</h2>` )
+          // if(!cmt.data.length && success === 0) $(".text-after-loading-cmt").prepend( `<h2 className="one">0</h2>` )
           setRe(re.data);
           setCmt(cmt.data);
         })
@@ -193,31 +194,7 @@ function MyPage() {
         <div className="section">
           <Container>
             <div className="button-container">
-              <Button className="btn-round" color="info" size="lg">
-                Follow
-              </Button>
-              <Button
-                className="btn-round btn-icon"
-                color="default"
-                id="tooltip515203352"
-                size="lg"
-              >
-                <i className="fab fa-twitter"></i>
-              </Button>
-              <UncontrolledTooltip delay={0} target="tooltip515203352">
-                Follow me on Twitter
-              </UncontrolledTooltip>
-              <Button
-                className="btn-round btn-icon"
-                color="default"
-                id="tooltip340339231"
-                size="lg"
-              >
-                <i className="fab fa-instagram"></i>
-              </Button>
-              <UncontrolledTooltip delay={0} target="tooltip340339231">
-                Follow me on Instagram
-              </UncontrolledTooltip>
+             <UpLoadImg></UpLoadImg>
             </div>
             <h3 className="title">About me</h3>
             <h5 className="description">
