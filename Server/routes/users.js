84fb39953ccc6f91bd194d1c20bff78af65d56f3
Dong Quang Quy <quangquya2@gmail.com>
2021-06-23 12:19:42 +0700

    add - update Avatar

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,33 +1,40 @@
-const router = require("express").Router();
-const userController = require("../app/controllers/UserController");
-const multer = require("multer");
-const fileUploader = require("../app/middleware/uploadMiddleware");
-const auth = require("../app/middleware/auth");
+const router = require('express').Router();
+const userController = require('../app/controllers/UserController');
+const multer = require('multer');
+const fileUploader = require('../app/middleware/uploadMiddleware');
+const auth = require('../app/middleware/auth');
 
 router.post(
-  "/register",
-  fileUploader.single("avatar"),
-  userController.register
+    '/register',
+    fileUploader.single('avatar'),
+    userController.register
+);
+
+router.patch(
+    '/avatar',
+    fileUploader.single('avatar'),
+    auth,
+    userController.updateAvatar
 );
 
 router.use(multer().none());
 
-router.post("/login", userController.login);
+router.post('/login', userController.login);
 
-router.get("/logout", userController.logout);
+router.get('/logout', userController.logout);
 
-router.get("/refresh_token", userController.refreshToken);
+router.get('/refresh_token', userController.refreshToken);
 
-router.get("/me", auth, userController.getMe);
+router.get('/me', auth, userController.getMe);
 
-router.get("/username/:username", userController.getByUsername);
+router.get('/username/:username', userController.getByUsername);
 
-router.get("/email/:email", userController.getByEmail);
+router.get('/email/:email', userController.getByEmail);
 
-router.get("/permission/:permission", userController.getByPermission);
+router.get('/permission/:permission', userController.getByPermission);
 
-router.get("/:id", userController.getById);
+router.get('/:id', userController.getById);
 
-router.get("/", userController.getAll);
+router.get('/', userController.getAll);
 
 module.exports = router;
