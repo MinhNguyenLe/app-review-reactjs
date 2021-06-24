a7f138a34125f81e11a5cafe4e75b113e0272466
ZinZinNguyen <71427292+ZinZinNguyen@users.noreply.github.com>
2021-06-24 12:07:05 +0700

    dm m

--- src/views/pages/Schools.js
+++ src/views/pages/Schools.js
@@ -14,8 +14,8 @@ function Schools() {
     document.body.classList.add("index-page");
     document.body.classList.add("sidebar-collapse");
     document.documentElement.classList.remove("nav-open");
-    // window.scrollTo(0, 0);
-    // document.body.scrollTop = 0;
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
     return function cleanup() {
       document.body.classList.remove("index-page");
       document.body.classList.remove("sidebar-collapse");
