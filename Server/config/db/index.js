a7a2f6c3433fe013b6767ed1965bf5213c00f7a5
Dong Quang Quy <quangquya2@gmail.com>
2021-07-05 10:01:20 +0700

    add - filter

--- Server/config/db/index.js
+++ Server/config/db/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URL;
+//const uri = process.env.MONGODB_URL;
+const uri = 'mongodb://localhost:27017/review-school';
 
 async function connect() {
     try {
--- Server/routes/schools.js
+++ Server/routes/schools.js
@@ -1,27 +1,28 @@
-const router = require("express").Router();
-const schoolController = require("../app/controllers/SchoolController");
-const multer = require("multer");
-const fileUploader = require("../app/middleware/uploadMiddleware");
+const router = require('express').Router();
+const schoolController = require('../app/controllers/SchoolController');
+const multer = require('multer');
+const fileUploader = require('../app/middleware/uploadMiddleware');
 
 router.post(
-  "/",
-  fileUploader.fields([
-    { name: "logo", maxCount: 1 },
-    { name: "gallery", maxCount: 10 },
-  ]),
-  schoolController.create
+    '/',
+    fileUploader.fields([
+        { name: 'logo', maxCount: 1 },
+        { name: 'gallery', maxCount: 10 },
+    ]),
+    schoolController.create
 );
 
 router.use(multer().none());
-router.get("/search/location", schoolController.searchByLocation);
-router.get("/search/code", schoolController.searchByCode);
+router.get('/search/location', schoolController.searchByLocation);
+router.get('/search/code', schoolController.searchByCode);
 
-router.get("/test", schoolController.test);
-router.get("/search", schoolController.searchByName);
-router.get("/:id", schoolController.getById);
-router.get("/:id/reviews", schoolController.getReviewsByIdSchool);
-router.get("/", schoolController.getAll);
-router.get("/:id/number-of-reviews", schoolController.countReviewsByIdSchool);
-router.get("/:id/type-of-schools", schoolController.filterByTypeOfSchool);
+router.get('/test', schoolController.test);
+router.get('/search', schoolController.searchByName);
+router.get('/filter', schoolController.filter);
+router.get('/:id', schoolController.getById);
+router.get('/:id/reviews', schoolController.getReviewsByIdSchool);
+router.get('/', schoolController.getAll);
+router.get('/:id/number-of-reviews', schoolController.countReviewsByIdSchool);
+router.get('/:id/type-of-schools', schoolController.filterByTypeOfSchool);
 
 module.exports = router;
