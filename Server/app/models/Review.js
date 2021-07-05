ed525bf63170a510576406359d08797c46eff233
Dong Quang Quy <quangquya2@gmail.com>
2021-07-05 18:02:03 +0700

    add - report

--- Server/app/models/Review.js
+++ Server/app/models/Review.js
@@ -1,42 +1,49 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const Review = new Schema(
-  {
-    idSchool: {
-      type: String,
-      required: true,
+    {
+        idSchool: {
+            type: String,
+            required: true,
+        },
+        idUser: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        ratePoint: {
+            type: Number,
+            required: true,
+        },
+        positive: {
+            type: String,
+        },
+        negative: {
+            type: String,
+        },
+        advice: {
+            type: String,
+        },
+        rateValue: {
+            up: {
+                count: { type: Number, default: 0 },
+                idUser: [String],
+            },
+            down: {
+                count: { type: Number, default: 0 },
+                idUser: [String],
+            },
+        },
+        report: {
+            reported: {
+                type: Boolean,
+                default: false,
+            },
+            content: [String],
+        },
     },
-    idUser: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-    ratePoint: {
-      type: Number,
-      required: true,
-    },
-    positive: {
-      type: String,
-    },
-    negative: {
-      type: String,
-    },
-    advice: {
-      type: String,
-    },
-    rateValue: {
-      up: {
-        count: { type: Number, default: 0 },
-        idUser: [String],
-      },
-      down: {
-        count: { type: Number, default: 0 },
-        idUser: [String],
-      },
-    },
-  },
-  { timestamps: true, collection: "reviews" }
+    { timestamps: true, collection: 'reviews' }
 );
 
-module.exports = mongoose.model("Review", Review);
+module.exports = mongoose.model('Review', Review);
